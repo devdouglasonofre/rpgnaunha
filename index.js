e6089241beceb5d0ad215bf76e6b326672fdf026
Douglas Onofre <douglas12psp@gmail.com>
2020-03-20 15:46:05 -0300

    versão 2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const FONT = "28px monospace"
+const WIDTH = 128
+const HEIGHT = 120
 
 // Frame do jogo
 let gFrame = 0
@@ -6,31 +8,36 @@ let gFrame = 0
 let gImgMap = 0
 
 const canvasGame = document.getElementById("tela")
+canvasGame.width = WIDTH
+canvasGame.height = HEIGHT
 const game = canvasGame.getContext("2d")
 
-function gameLoop(){
-  // Adiciona um frame a cada iteração
-  gFrame++
-
+function draw() {
   // Desenha imagens na tela
-  for (let y = 0;  y< 16; y++){
-    for (let x = 0; x< 16; x++){
-      game.drawImage(gImgMap, x*32, y*32)
+  for (let y = 0; y < 16; y++) {
+    for (let x = 0; x < 16; x++) {
+      game.drawImage(gImgMap, x * 32, y * 32)
     }
   }
 
   // Escreve o texto na tela
   game.font = FONT
 
-  // 
-  game.fillText("Hello World" + gFrame, gFrame/10, 64)
+  // Escreve o texto na tela
+  game.fillText("Hello World" + gFrame, gFrame / 10, 64)
 }
 
-function iniciarJogo(){
+function update() {
+  // Adiciona um frame a cada iteração
+  gFrame++
+  draw()
+}
+
+function iniciarJogo() {
   gImgMap = new Image()
   gImgMap.src = "img/map.png"
   setInterval(() => {
-    gameLoop()
+    update()
   }, 33)
 }
 
