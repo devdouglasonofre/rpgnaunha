19d19536243b9f1e66a037a89f88d6baca68a303
Douglas Onofre <douglas12psp@gmail.com>
2020-03-20 16:57:02 -0300

    Versão 0.04

--- index.js
+++ index.js
@@ -1,41 +1,118 @@
 const FONT = "28px monospace"
 const WIDTH = 128
 const HEIGHT = 120
+const MAP_WIDTH = 32
+const MAP_HEIGHT = 32
+const TILEROW = 4
+const TILECOLUMN = 4
+const TILESIZE = 8
+
+const mapaMundi = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+  0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6, 13, 6, 0, 0, 0,
+  0, 3, 3, 10, 11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+  0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3, 12, 3, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+  7, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+  7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+]
 
 // Frame do jogo
 let gFrame = 0
 // Map da Imagem 
 let gImgMap = 0
+// 
+let gScreen
+let gWidth
+let gHeight
 
+// Tela virtual
+gScreen = document.createElement("canvas")
+gContext = gScreen.getContext("2d")
+gScreen.width = WIDTH
+gScreen.height = HEIGHT
+
+// Tela "real" 
 const canvasGame = document.getElementById("tela")
-canvasGame.width = WIDTH
-canvasGame.height = HEIGHT
+canvasGame.width = window.innerWidth
+canvasGame.height = window.innerHeight
+
+// Faz com que a tela real não suavize os pixels
 const game = canvasGame.getContext("2d")
+game.imageSmoothingEnabled = false
+
+// Pega o tamanho da tela
+gWidth = canvasGame.width
+gHeight = canvasGame.height
+
+// Mantém o AspectRation 
+if (gWidth / WIDTH < gHeight / HEIGHT) {
+  gHeight = gWidth * HEIGHT / WIDTH;
+} else {
+  gWidth = gHeight * WIDTH / HEIGHT;
+}
 
 function draw() {
+  // Transfere o conteúdo da tela virtual para a tela principal
+  game.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight)
+
   // Desenha imagens na tela
-  for (let y = 0; y < 16; y++) {
-    for (let x = 0; x < 16; x++) {
-      game.drawImage(gImgMap, x * 32, y * 32)
+  for (let x = 0; x < 32; x++) {
+    for (let y = 0; y < 32; y++) {
+      desenharTile(gContext, x * TILESIZE, y * TILESIZE, mapaMundi[x + y * MAP_WIDTH])
     }
   }
+}
 
-  // Escreve o texto na tela
-  game.font = FONT
-
-  // Escreve o texto na tela
-  game.fillText("Hello World" + gFrame, gFrame / 10, 64)
+function desenharTile(contexto, x, y, idTile) {
+  // Pega o tile correto baseado em seu número no mapa
+  // No caso de X, ele pega o numero no mapa, ex 0,
+  // divide pelo numero de tiles por coluna ( 4, nesse caso )
+  // e pega a sobra. Nesse caso, o resto é 0, então será o primeiro de coluna
+  let idX = (idTile % TILEROW) * TILESIZE
+  // Já com o Y, ele fará a mesma divisão, mas dessa vez será arredondado para
+  // para pegar o tile em relação à linha.
+  let idY = Math.floor(idTile / TILECOLUMN) * TILESIZE
+  // E, por fim, irá desenhar na tela.
+  contexto.drawImage(gImgMap, idX, idY, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE)
 }
 
 function update() {
   // Adiciona um frame a cada iteraÃ§Ã£o
   gFrame++
+
   draw()
 }
 
 function iniciarJogo() {
   gImgMap = new Image()
   gImgMap.src = "img/map.png"
+
   setInterval(() => {
     update()
   }, 33)
